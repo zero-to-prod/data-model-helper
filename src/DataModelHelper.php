10df5fcaeca953761e63c0299d3c998ff8adcc9e
david_smith <david_smith@sweetwater.com>
2024-10-14 11:01:46 -0400

    Add docblock to `DataModelHelper.php`.

--- src/DataModelHelper.php
+++ src/DataModelHelper.php
@@ -7,6 +7,44 @@ use ReflectionProperty;
 
 trait DataModelHelper
 {
+    /**
+     * Maps an array of values to instances of a specified type.
+     *
+     * ```
+     * readonly class User
+     * {
+     *  use \Zerotoprod\DataModel\DataModel;
+     *  use \Zerotoprod\DataModelHelper\DataModelHelper;
+     *
+     *  #[Describe([
+     *      'cast'    => [DataModelHelper::class, 'mapOf'], // Casting method to use
+     *      'type'    => Alias::class,                      // Target type for each item
+     *      'coerce'  => true,                              // Coerce single elements into an array
+     *      'using'   => [User::class, 'map'],              // Custom mapping function
+     *      'map_via' => 'mapper',                          // Custom mapping method (defaults to 'map')
+     *  ])]
+     *  public Collection $Aliases;
+     *
+     *  public static function map(array $values): Collection
+     *  {
+     *      $Collection = new Collection();
+     *      $Collection->items = $values;
+     *
+     *      return $Collection;
+     *  }
+     * }
+     *
+     * readonly class Alias
+     * {
+     *  use \Zerotoprod\DataModel\DataModel;
+     *
+     *  public string $name;
+     * }
+     * ```
+     *
+     * @link https://github.com/zero-to-prod/data-model-helper
+     * @see  https://github.com/zero-to-prod/data-model
+     */
     public static function mapOf(mixed $value, array $context, ?ReflectionAttribute $Attribute, ReflectionProperty $Property)
     {
         $args = $Attribute?->getArguments();
